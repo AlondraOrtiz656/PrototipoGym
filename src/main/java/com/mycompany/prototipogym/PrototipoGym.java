b25782e1ae7b662c9b00c56af3aeec7860b55873
asist-depti <asist-depti@asist-depti.TROQUELADOS.LOCAL>
2025-03-26 10:21:49 -0400

    Carga incial

---
+++ src/main/java/com/mycompany/prototipogym/PrototipoGym.java
@@ -0,0 +1,12 @@
+package com.mycompany.prototipogym;
+
+/**
+ *
+ * @author Alondra & Carlos
+ */
+public class PrototipoGym {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
