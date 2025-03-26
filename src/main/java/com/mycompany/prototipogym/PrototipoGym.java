9a02579f92192d203a53eee7afba3a5a2cd187b7
asist-depti <asist-depti@asist-depti.TROQUELADOS.LOCAL>
2025-03-26 10:56:31 -0400

    Prueba de comit, cambio del sout

--- src/main/java/com/mycompany/prototipogym/PrototipoGym.java
+++ src/main/java/com/mycompany/prototipogym/PrototipoGym.java
@@ -7,6 +7,6 @@ package com.mycompany.prototipogym;
 public class PrototipoGym {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("PrototipoGym");
     }
 }
